d633ffbe4a449ef4560144b7cdbd918b74e58150
LeonBre <leon@brettin.de>
2016-08-08 16:45:41 +0200

    Add some test values for the database.

--- Server-Mitfahren/src/main/java/helper/DatabaseHelper.java
+++ Server-Mitfahren/src/main/java/helper/DatabaseHelper.java
@@ -27,11 +27,29 @@ public class DatabaseHelper {
 
 
 	public void storeTestData() {
-		MitfahrenUser testUser = new MitfahrenUser("Max Mustermann", "1234", "5678");
-		userService.persists(testUser);
+		MitfahrenUser testUser1 = new MitfahrenUser("Max Mustermann", "1234", "046334567");
+		MitfahrenUser testUser2 = new MitfahrenUser("Netter Fahrer", "5678", "017011223344");
+		MitfahrenUser testUser3 = new MitfahrenUser("Klaus Kleber", "91011", "0160123456");
+		MitfahrenUser testUser4 = new MitfahrenUser("Karla Kolumna", "1357", "18027235");
+		userService.persists(testUser1);
+		userService.persists(testUser2);
+		userService.persists(testUser3);
+		userService.persists(testUser4);
 		Calendar nowCalendar = Calendar.getInstance();
-		Drive drive = new Drive("Braunschweig", "Hannover", 
-				nowCalendar,testUser);
-		driveService.persists(drive);
+		Drive drive1 = new Drive("Braunschweig", "Hannover", 
+				nowCalendar,testUser1);
+		Drive drive2 = new Drive("Gießen", "Lübeck", 
+				nowCalendar,testUser1);
+		Drive drive3 = new Drive("Berlin", "München", 
+				nowCalendar,testUser1);
+		Drive drive4 = new Drive("Braunschweig", "Hannover", 
+				nowCalendar,testUser1);
+		Drive drive5 = new Drive("Hannover", "Braunschweig", 
+				nowCalendar,testUser1);
+		driveService.persists(drive1);
+		driveService.persists(drive2);
+		driveService.persists(drive3);
+		driveService.persists(drive4);
+		driveService.persists(drive5);
 	}
 }
