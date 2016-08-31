f56d4d2b27d5247670ca7f1e816c35f477a903b0
LeonBre <leon@brettin.de>
2016-08-31 15:57:50 +0200

    Add logic for Register a new User.

--- Server-Mitfahren/src/main/java/apiv1/converters/RegistrationConverter.java
+++ Server-Mitfahren/src/main/java/apiv1/converters/RegistrationConverter.java
@@ -2,6 +2,7 @@ package apiv1.converters;
 
 import apiv1.models.request.RegisterUserModel;
 import apiv1.models.response.RegisterUserResponse;
+import entities.MitfahrenUser;
 import entities.MitfahrenUserService;
 
 public class RegistrationConverter {
@@ -13,14 +14,33 @@ public class RegistrationConverter {
 	}
 	
 	public RegisterUserResponse convert(RegisterUserModel userModel) {
-		boolean phoneIsValid = userService.findPhoneNumber(userModel.phone) == null;
+		RegisterUserResponse response = new RegisterUserResponse();
 		
-		if(userService.find(userModel.username) == null 
-				&& userService.findMail(userModel.mail) == null 
-				&& phoneIsValid) {
+		boolean usernameIsValid = userService.find(userModel.username) == null;
+		boolean mailIsValid = userService.findMail(userModel.mail) == null 
+				&& userModel.mail.contains("@") 
+				&& userModel.mail.contains(".");
+		boolean phoneIsValid = userModel.phone != "110" 
+				&& userModel.phone != "112" 
+				&& userService.findPhoneNumber(userModel.phone) == null;
+		
+		
+		boolean registrationIsPossible = usernameIsValid && phoneIsValid && mailIsValid;
+		
+		if(registrationIsPossible) 
+		{
+			MitfahrenUser newUser = new MitfahrenUser(userModel.username, 
+					userModel.password, userModel.phone, userModel.mail);
+			userService.persists(newUser);
 			
+			response.isRegistered = true;
+			response.userId = newUser.getUserId();
+		} else 
+		{
+			response.isRegistered = false;
+			response.userId = 0;
 		}
 		
-		return null;
+		return response;
 	}
 }
