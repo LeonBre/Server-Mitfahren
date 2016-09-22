2d81da6947459c6de5370d6ef2652f51aebf086e
LeonBre <leon@brettin.de>
2016-09-22 17:49:16 +0200

    Comment AuthenticationConverter.java and Authenticationvalidator.java.

--- Server-Mitfahren/src/main/java/apiv1/converters/AuthenticationConverter.java
+++ Server-Mitfahren/src/main/java/apiv1/converters/AuthenticationConverter.java
@@ -6,14 +6,32 @@ import apiv1.validator.AuthenticationValidator;
 import entities.MitfahrenUser;
 import entities.MitfahrenUserService;
 
+/**
+ * Converter Class for the Authentication of an user.
+ * @author Leon Johann Brettin
+ *
+ */
 public class AuthenticationConverter {
 
 	MitfahrenUserService userService;
 	
+	/**
+	 * Basic Constructor for the Authentication Converter.
+	 * @param userService UserService, to get database access to the users.
+	 */
 	public AuthenticationConverter(MitfahrenUserService userService) {
 		this.userService = userService;
 	}
 	
+	/**
+	 * Convertrs a Authentication User Model request to an AuthenticationUserResponse.
+	 * The authentication data will be checked against the saved data in the database.
+	 * If the user is validated right, and the password and the username are correct the model will respond with a
+	 * positive AuthenticationUserRespone,
+	 * else it will respond with a negative respond.
+	 * @param userModel Usermodel of the Authentication Request.
+	 * @return AuthenticationUserRespond of the Authentication of the user.
+	 */
 	public AuthenticateUserResponse convertModelToResponse(AuthenticateUserModel userModel) {
 		MitfahrenUser user = userService.find(userModel.username);
 		AuthenticateUserResponse response = new AuthenticateUserResponse();
--- Server-Mitfahren/src/main/java/apiv1/validator/AuthenticationValidator.java
+++ Server-Mitfahren/src/main/java/apiv1/validator/AuthenticationValidator.java
@@ -4,8 +4,24 @@ import apiv1.models.request.AuthenticateUserModel;
 import entities.MitfahrenUser;
 import entities.MitfahrenUserService;
 
+/**
+ * Validation helper class for the Authentication process.
+ * @author Leon Johann Brettin
+ *
+ */
 public class AuthenticationValidator {
 
+	/**
+	 * Checks if the given input is right, so that the user can log in successfully.
+	 * It checks a Mitfahren user of the database against an AuthencticationUserModel request.
+	 * The validator checks:
+	 * - if the username is the same as in the usermodel.
+	 * - if the hashed password is the same as in the usermodel.
+	 * - if the user is already activated.
+	 * @param user Mitfahrenuser of the database.
+	 * @param userModel Usermodel of the request.
+	 * @return True, when the authentication is succesfull, false if not.
+	 */
 	public static boolean authenticateUser(MitfahrenUser user, AuthenticateUserModel userModel) {
 		if(user == null)
 			return false;
