296c62f89c0c593b765972b6242dd036922b7a6c
LeonBre <leon@brettin.de>
2016-10-07 18:52:49 +0200

    Add Response model for userPageInfo.

--- Server-Mitfahren/src/main/java/apiv1/models/response/UserPageInfoResponse.java
+++ Server-Mitfahren/src/main/java/apiv1/models/response/UserPageInfoResponse.java
@@ -1,5 +1,15 @@
 package apiv1.models.response;
 
+import java.util.List;
+
+import apiv1.models.response.helpermodel.UserPageInfoDrive;
+
 public class UserPageInfoResponse {
 
+	public String username;
+	public String rating;
+	public String phoneNumber;
+	public String mail;
+	public List <UserPageInfoDrive> drive;
 }
+
---
+++ Server-Mitfahren/src/main/java/apiv1/models/response/helpermodel/UserPageInfoDrive.java
@@ -0,0 +1,11 @@
+package apiv1.models.response.helpermodel;
+
+public class UserPageInfoDrive {
+
+	public String destination;
+	public String arrival;
+	public int driverCount;
+	public String price;
+	
+	public UserPageInfoDrive() {}
+}
