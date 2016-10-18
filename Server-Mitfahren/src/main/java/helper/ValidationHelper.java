065435c65743bc7b9df1d04ed5117cd7bcf45152
LeonBre <leon@brettin.de>
2016-10-18 18:52:16 +0200

    Comment ValidationHelper.java

--- Server-Mitfahren/src/main/java/helper/ValidationHelper.java
+++ Server-Mitfahren/src/main/java/helper/ValidationHelper.java
@@ -3,14 +3,26 @@ package helper;
 import entities.MitfahrenUser;
 import entities.MitfahrenUserService;
 
+/**
+ * Helper class to validate a succesfull login of an user.
+ * 
+ * @author Leon Johann Brettin
+ *
+ */
 public class ValidationHelper {
 
+	/**
+	 * Validates an user against an already saved user of the Database.
+	 * 
+	 * @param userService Userservice to get an user out of the database.
+	 * @param username Input username of the client.
+	 * @param password Input hashed password of the client.
+	 * @param userId Input userId of the client.
+	 * @return True, when the user is successfully validated.
+	 */
 	public static boolean validateUser(MitfahrenUserService userService, String username, String password, String userId) {
 		try {
 			MitfahrenUser validationUser = userService.find(Integer.parseInt(userId));
-			System.out.println("Username: " + validationUser.getUsername().equals(username));
-			System.out.println(validationUser.getUsername());
-			System.out.println(username);
 			return validationUser.getUsername().equals(username) 
 					&& validationUser.getHashPassword().equals(password);
 						
